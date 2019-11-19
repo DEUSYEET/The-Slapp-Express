a1c681fe13910cc1f500a60c9c51143d5244cfab
WinnieQueen <47342826+WinnieQueen@users.noreply.github.com>
2019-11-19 14:19:04 -0700

    session stoof

--- index.js
+++ index.js
@@ -10,24 +10,36 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const checkAuth = (req, res, next) => {
+    console.log(req.session.user);
+    if(req.session.user && req.session.user.isAuthenticated) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname + "/public")));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'mY53cr3tPa$$word45',
+    saveUninitialized: true,
+    resave: true
+}))
 
 var urlencodedParser = bodyParser.urlencoded({
     extended: true
-})
-
+});
 
 //Routes
 app.get('/', route.login);
-app.get('/main', route.index);
+app.get('/index', checkAuth, route.index);
 app.get('/logout', route.logout);
 app.get('/create', route.create);
 app.post('/create', urlencodedParser, route.createUser);
-app.post('/login', urlencodedParser, route.index);
+app.post('/login', urlencodedParser, route.loginUser);
 
 //-End Routes
 
